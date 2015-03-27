1f334da27fcf8158b7a2a28889df8be933aa5d19
Matt Eaton <matt@Syros-Mac-mini.local>
2015-03-27 18:32:33 -0400

    Added CV vs bootstrap comparison.

--- programs/2-methods/01-regression.R
+++ programs/2-methods/01-regression.R
@@ -247,12 +247,48 @@ summary(lm(SerumInsulin ~ PlasmaGlucose ,data=diabetes))$coef
 ## that may not hold
 
 #################################################
+## bootstrap estimate for correlation between Plasma Glucose Level and Serum
+plot(diabetes$SerumInsulin,diabetes$PlasmaGlucose)
+COR <- cor(diabetes$SerumInsulin,diabetes$PlasmaGlucose)
+COR.closedForm.SE <- sqrt((1-COR^2) / (nrow(diabetes) - 2))
+
+boot.fn <- function(data, index) return(cor(data$SerumInsulin[index],data$PlasmaGlucose[index]))
+set.seed(1)
+boot(diabetes,boot.fn,1000)
+
+#################################################
 ## bootstrap estimates for AUC
-## NESTED!!!!
+diabetes$CV.index <- rep(1:10,length=nrow(diabetes))
+boot.fn <- function(data, index) {
+  diabetes.inner <- data[index,]
+  for(i in 1:10) {
+    cat("  ",i," / ",10,"  \r")
+    in.fold <- diabetes.inner$CV.index != i
+    glm.fit.cv <- glm(Diabetes ~ Age + PlasmaGlucose,data=diabetes.inner[in.fold,],family='binomial')
+    p <- predict(glm.fit.cv,type='response',newdata=diabetes.inner[!in.fold,])
+    diabetes.inner[!in.fold,'prediction'] <- p
+  }
+  pred <- prediction(diabetes.inner$prediction,diabetes.inner$Diabetes)
+  return(performance(pred,measure='auc')@y.values[[1]])
+}
+set.seed(1)
+cv.boot <- boot(diabetes,boot.fn,1000)
 
+cv.aucs <- vector()
+for(j in 1:1000) {
+  diabetes$CV.index <- sample(rep(1:10,length=nrow(diabetes)))
+  for(i in 1:10) {
+    cat("  ",i," / ",10,"  \r")
+    in.fold <- diabetes$CV.index != i
+    glm.fit.cv <- glm(Diabetes ~ Age + PlasmaGlucose,data=diabetes[in.fold,],family='binomial')
+    p <- predict(glm.fit.cv,type='response',newdata=diabetes[!in.fold,])
+    diabetes[!in.fold,'prediction'] <- p
+  }
+  pred <- prediction(diabetes$prediction,diabetes$Diabetes)
+  cv.aucs[j] <- performance(pred,measure='auc')@y.values[[1]]
+}
 
-
-
+cv.stderr <- sd(cv.aucs) / sqrt(nrow(diabetes))
 
 ##cn <- c("word_freq_make","word_freq_address","word_freq_all","word_freq_3d","word_freq_our","word_freq_over","word_freq_remove","word_freq_internet","word_freq_order","word_freq_mail","word_freq_receive","word_freq_will","word_freq_people","word_freq_report","word_freq_addresses","word_freq_free","word_freq_business","word_freq_email","word_freq_you","word_freq_credit","word_freq_your","word_freq_font","word_freq_000","word_freq_money","word_freq_hp","word_freq_hpl","word_freq_george","word_freq_650","word_freq_lab","word_freq_labs","word_freq_telnet","word_freq_857","word_freq_data","word_freq_415","word_freq_85","word_freq_technology","word_freq_1999","word_freq_parts","word_freq_pm","word_freq_direct","word_freq_cs","word_freq_meeting","word_freq_original","word_freq_project","word_freq_re","word_freq_edu","word_freq_table","word_freq_conference","char_freq_;","char_freq_(","char_freq_[","char_freq_!","char_freq_$","char_freq_#","capital_run_length_average","capital_run_length_longest","capital_run_length_total","spam")
 ##spam <- read.csv("data/spambase.data.txt")
